3106574d6e37d34288d0338792b0cc33d1f87815
Lyon_Yan <765211630@qq.com>
2015-11-21 11:29:19 +0800

    update

---
+++ gen/org/lyon_yan/android/lib_FontAwesome/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.lyon_yan.android.lib_FontAwesome;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/org/lyon_yan/android/lib_FontAwesome/FontManager.java
+++ src/org/lyon_yan/android/lib_FontAwesome/FontManager.java
@@ -19,6 +19,10 @@ public class FontManager {
 	public static final String ROOT = "fonts/", FONTAWESOME = ROOT
 			+ "fontawesome-webfont.ttf";
 
+	public static Typeface getTypeface(Context context) {
+		return Typeface.createFromAsset(context.getAssets(), FONTAWESOME);
+	}
+
 	public static Typeface getTypeface(Context context, String font) {
 		return Typeface.createFromAsset(context.getAssets(), font);
 	}
